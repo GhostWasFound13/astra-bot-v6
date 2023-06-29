f61c41ac4bd2d935e6e0347a117b82f11071e643
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-06-29 14:01:29 +0700

    Changed to logger

--- src/handler/clientReady.js
+++ src/handler/clientReady.js
@@ -5,7 +5,7 @@ const { Group } = require("@akarui/structures");
 const fs = require('fs');
 const path = require('path');
 const config = require("./config.js");
-
+const logger = require("./plugin/logger.js")
 module.exports = async (client, voice) => {
 
   if (config.replit.usingReplit === true) {
@@ -16,7 +16,7 @@ module.exports = async (client, voice) => {
     });
     app.listen(config.replit.port, () => {
         if (config.disableLogs === false) {
-        console.log("\x1B[90m[REPLIT]\x1B[0m Successfully established connection to port \x1B[32m" + config.replit.port + "\x1B[0m");
+        logger.info("\x1B[90m[REPLIT]\x1B[0m Successfully established connection to port \x1B[32m" + config.replit.port + "\x1B[0m");
         }
     });
   }
@@ -48,12 +48,12 @@ module.exports = async (client, voice) => {
         console.warn("\n" + "\x1B[90m[CLIENT]\x1B[0m Loaded " + client.cmd.default.size + (client.cmd.default.size >= 2 ? " commands" : " command" + "."));
         console.log("\x1B[90m[CLIENT]\x1B[0m Successfully initialized on \x1B[32m" + client.user.username + "\x1B[0m");
         if (config.disableInvite === false) {
-        console.log(`\x1B[90m[INFO]\x1B[0m Invite your bot with this link: https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=0&scope=bot%20applications.commands`);
+       logger.info(`\x1B[90m[INFO]\x1B[0m Invite your bot with this link: https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=0&scope=bot%20applications.commands`);
         }
         try {
           if (Object.keys(require("./auth.json")).length === 0) {
             setTimeout(() => {
-              console.log("\x1B[90m[INFO]\x1B[0m \x1B[31mEnsure you follow the Device Auth Instructions above, or your bot may crash after some time.\x1B[0m");
+              logger.info("\x1B[90m[INFO]\x1B[0m \x1B[31mEnsure you follow the Device Auth Instructions above, or your bot may crash after some time.\x1B[0m");
             }, 2500);
           }
         } catch (err) {
@@ -63,11 +63,11 @@ module.exports = async (client, voice) => {
         }        
       });
       if (config.disableLogs === false) {
-      console.log("\x1B[90m[CLIENT]\x1B[0m \x1B[32mAttempting to load commands\x1B[0m");
+      logger.info("\x1B[90m[CLIENT]\x1B[0m \x1B[32mAttempting to load commands\x1B[0m");
       }
       await loader.load(client.cmd, config.commands); // Location for your commands.
       if (config.disableLogs === false) {
-      console.log("\n\x1B[90m[CLIENT]\x1B[0m \x1B[32mAttempting to load voice events\x1B[0m");
+      logger.info("\n\x1B[90m[CLIENT]\x1B[0m \x1B[32mAttempting to load voice events\x1B[0m");
       }
       await loader.load(voice.cmds, config.voiceCommands); // Location for your @akarui/aoi.music events, NOT for commands.
 
@@ -76,12 +76,12 @@ module.exports = async (client, voice) => {
       }
 
       if (config.disableLogs === false) {
-      console.log("\n\x1B[90m[CLIENT]\x1B[0m \x1B[32mAttempting to load custom functions\x1B[0m");
+      logger.info("\n\x1B[90m[CLIENT]\x1B[0m \x1B[32mAttempting to load custom functions\x1B[0m");
       }
       await loader.load(client.customFunctions, config.customCommands);
       Object.values(client.customFunctions).forEach(x => client.functionManager.createFunction(...x.allValues()));
 } catch (err) {
-    console.warn("\x1B[90m[ERR]\x1B[0m \x1B[31mFailed to load commands and/or events, please try again.\x1B[0m");
+    logger.warn("\x1B[90m[ERR]\x1B[0m \x1B[31mFailed to load commands and/or events, please try again.\x1B[0m");
     console.error(err)
 }
 await require(config.variables)(client);
