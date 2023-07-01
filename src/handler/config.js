0f96bae9dd7f2675708f8e4b37fd896f1a424750
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-07-01 13:48:38 +0700

    Update config.js

--- src/handler/config.js
+++ src/handler/config.js
@@ -17,13 +17,29 @@ module.exports = {
     "events": ["onMessage", "onInteractionCreate"], // Array of events used for your bot, list can be found here: https://aoi.js.org/docs/guides/events
     "commands": "../../commands", // Location of your commands directory.
     "voiceCommands": "./handler/voice",
-    "customCommands": "./handler//customFunctions", // Location of your custom commands directory.
+    "customCommands": "./handler/customFunctions", // Location of your custom commands directory.
     "variables": "./variables.js", // Location of your variables file, note that it's trying to search the file from the clientReady.js file.
 
     /* - @akarui/aoi.music setup - */
     "soundcloudID": "", // Optional, add a soundcloud ID. Don't know how? https://aoi.js.org/docs/other/soundcloudid
     "youtubeGl": "US", // Define the location of the aoi.music client.
-
+  "panel":{
+    username: process.env["username"],//panel login username
+    password: process.env["password"],//panel login password
+    secret: require('crypto').randomBytes(16).toString("hex"),
+    port: 3000,//panel port
+    mainFile: "index.js",//code mainfile name
+    commands: "../../commands/",//message commands folder
+    interaction:"../../commands/slash/",//slash commands folder
+    theme:"orange",//panel & dashboard theme
+    codetheme:"gruvbox-dark",//code editor theme
+    customIndex:"adminLogin"
+  },
+  "dash":{
+    clientId:"1048543886156501022",
+    clientSecret:process.env.secret,//available in discord.dev portal
+    redirectUri:"https://aoijs-advanced.blurr1447.repl.co/redirect",//needs to be updated on discord dev portal too!
+  },
     /* - Custom Settings - */
     "replit": {
         "usingReplit": false,
