7fb72fa82ec76141f652cf6ecd2c6ee81c9cb1aa
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-07-01 12:47:10 +0700

    Alr of course didnt work

--- src/handler/clientReady.js
+++ src/handler/clientReady.js
@@ -5,7 +5,7 @@ const { Group } = require("@akarui/structures");
 const fs = require('fs');
 const path = require('path');
 const config = require("./config.js");
-const logger = require("./plugin/logger.js")
+
 module.exports = async (client, voice) => {
 
   if (config.replit.usingReplit === true) {
@@ -16,7 +16,7 @@ module.exports = async (client, voice) => {
     });
     app.listen(config.replit.port, () => {
         if (config.disableLogs === false) {
-        logger.info("\x1B[90m[REPLIT]\x1B[0m Successfully established connection to port \x1B[32m" + config.replit.port + "\x1B[0m");
+        console.log("\x1B[90m[REPLIT]\x1B[0m Successfully established connection to port \x1B[32m" + config.replit.port + "\x1B[0m");
         }
     });
   }
@@ -45,29 +45,29 @@ module.exports = async (client, voice) => {
       },
     });
     client.once('ready', () => {
-        logger.warn("\n" + "\x1B[90m[CLIENT]\x1B[0m Loaded " + client.cmd.default.size + (client.cmd.default.size >= 2 ? " commands" : " command" + "."));
-        logger.info("\x1B[90m[CLIENT]\x1B[0m Successfully initialized on \x1B[32m" + client.user.username + "\x1B[0m");
+        console.warn("\n" + "\x1B[90m[CLIENT]\x1B[0m Loaded " + client.cmd.default.size + (client.cmd.default.size >= 2 ? " commands" : " command" + "."));
+        console.log("\x1B[90m[CLIENT]\x1B[0m Successfully initialized on \x1B[32m" + client.user.username + "\x1B[0m");
         if (config.disableInvite === false) {
-       logger.info(`\x1B[90m[INFO]\x1B[0m Invite your bot with this link: https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=0&scope=bot%20applications.commands`);
+        console.log(`\x1B[90m[INFO]\x1B[0m Invite your bot with this link: https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=0&scope=bot%20applications.commands`);
         }
         try {
           if (Object.keys(require("./auth.json")).length === 0) {
             setTimeout(() => {
-              logger.info("\x1B[90m[INFO]\x1B[0m \x1B[31mEnsure you follow the Device Auth Instructions above, or your bot may crash after some time.\x1B[0m");
+              console.log("\x1B[90m[INFO]\x1B[0m \x1B[31mEnsure you follow the Device Auth Instructions above, or your bot may crash after some time.\x1B[0m");
             }, 2500);
           }
         } catch (err) {
           setTimeout(() => {
-            logger.error("\x1B[90m[ERR]\x1B[0m \x1B[31mFailed to check if you authenticated with the google device link.\x1B[0m");
+            console.error("\x1B[90m[ERR]\x1B[0m \x1B[31mFailed to check if you authenticated with the google device link.\x1B[0m");
           }, 2500);
         }        
       });
       if (config.disableLogs === false) {
-      logger.info("\x1B[90m[CLIENT]\x1B[0m \x1B[32mAttempting to load commands\x1B[0m");
+      console.log("\x1B[90m[CLIENT]\x1B[0m \x1B[32mAttempting to load commands\x1B[0m");
       }
       await loader.load(client.cmd, config.commands); // Location for your commands.
       if (config.disableLogs === false) {
-      logger.info("\n\x1B[90m[CLIENT]\x1B[0m \x1B[32mAttempting to load voice events\x1B[0m");
+      console.log("\n\x1B[90m[CLIENT]\x1B[0m \x1B[32mAttempting to load voice events\x1B[0m");
       }
       await loader.load(voice.cmds, config.voiceCommands); // Location for your @akarui/aoi.music events, NOT for commands.
 
@@ -76,13 +76,13 @@ module.exports = async (client, voice) => {
       }
 
       if (config.disableLogs === false) {
-      logger.info("\n\x1B[90m[CLIENT]\x1B[0m \x1B[32mAttempting to load custom functions\x1B[0m");
+      console.log("\n\x1B[90m[CLIENT]\x1B[0m \x1B[32mAttempting to load custom functions\x1B[0m");
       }
       await loader.load(client.customFunctions, config.customCommands);
       Object.values(client.customFunctions).forEach(x => client.functionManager.createFunction(...x.allValues()));
 } catch (err) {
-    logger.warn("\x1B[90m[ERR]\x1B[0m \x1B[31mFailed to load commands and/or events, please try again.\x1B[0m");
-    logger.error(err)
+    console.warn("\x1B[90m[ERR]\x1B[0m \x1B[31mFailed to load commands and/or events, please try again.\x1B[0m");
+    console.error(err)
 }
 await require(config.variables)(client);
 module.exports = client;
