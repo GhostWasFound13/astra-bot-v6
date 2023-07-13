ac0a2f5729c5aa52d95c1cd0d1ca70ab3baae189
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-07-13 10:22:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const { Util } = require("aoi.js");
 const { parse, createAst } = require("@akarui/aoi.parser");
 const { parseExtraOptions, parseComponents } = require("@akarui/aoi.parser/components");
 const fs = require('fs');
+const fetch = require("node-fetch");
+
 /* Requiring the config file with your bot's information in it. */
 
 const config = require("./handler/config.js");
@@ -76,6 +78,75 @@ async function ImportantFunction(){
     await bot.db.set("main","prefix",servers[i].id,orig.value)
   }
 }
+/* functions */
+
+const getCountriesData = async () => {
+  try {
+    const response = await fetch(
+      "https://www.jsonkeeper.com/b/L23E"
+    );
+    const data = await response.json();
+    return data.countries;
+  } catch (error) {
+    console.error("An error occurred while fetching country data:", error);
+    return [];
+  }
+};
+
+bot.functionManager.createFunction({
+  name: "$country",
+  type: "djs",
+  code: async (d) => {
+    const data = d.util.aoiFunc(d);
+    const [countryName, format] = data.inside.splits;
+
+    const countries = await getCountriesData();
+
+    const country = countries.find(
+      (c) =>
+        c.name_en.toLowerCase() === countryName.toLowerCase() ||
+        c.name_es.toLowerCase() === countryName.toLowerCase()
+    );
+
+    if (!country) {
+      data.result = "Invalid country specified!";
+      return { code: d.util.setCode(data) };
+    }
+
+    if (!format) {
+      data.result = "No format specified!";
+      return { code: d.util.setCode(data) };
+    }
+
+    const placeholders = {
+      "{name_en}": country.name_en,
+      "{name_es}": country.name_es,
+      "{continent_en}": country.continent_en,
+      "{continent_es}": country.continent_es,
+      "{capital_en}": country.capital_en,
+      "{capital_es}": country.capital_es,
+      "{dial_code}": country.dial_code,
+      "{code_2}": country.code_2,
+      "{code_3}": country.code_3,
+      "{tld}": country.tld,
+      "{km2}": country.km2,
+      "{flag}": `:flag_${country.code_2.toLowerCase()}:`,
+      "{image}": `https://flagcdn.com/w2560/${country.code_2.toLowerCase()}.jpg`,
+    };
+
+    let result = format;
+    for (const placeholder in placeholders) {
+      result = result.replace(
+        new RegExp(placeholder, "g"),
+        placeholders[placeholder]
+      );
+    }
+
+    data.result = result;
+
+    return { code: d.util.setCode(data) };
+  },
+});
 
 setInterval(ImportantFunction,1000);
 /* INITIALIZING PANEL */
