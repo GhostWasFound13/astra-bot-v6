856989f21527df6bc33f03846435aea09b133804
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-07-13 14:19:55 +0700

    Create TrackEnd.js

---
+++ src/voice/TrackEnd.js
@@ -0,0 +1,13 @@
+module.exports = {
+    type: "trackEnd",
+    channel: "$channelID",
+    code: `
+  $deleteMessage[$getGuildVar[music_player];$getGuildVar[music_channelID]]
+  $onlyIf[$channelExists[$getGuildVar[music_channelID]]==true;]
+  $onlyIf[$messageExists[$getGuildVar[music_player]]==true;]
+  
+  $title[🎵 Music End🎵 ]
+  $color[Green]
+
+      `
+}
