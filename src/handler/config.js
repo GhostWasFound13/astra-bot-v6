fba694dedfc57b4975db9b129ea29de526dddf74
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-07-14 15:26:06 +0700

    Update config.js

--- src/handler/config.js
+++ src/handler/config.js
@@ -11,7 +11,7 @@ module.exports = {
 
       - aoi.js setup (complicated) -
 */
-    "token": "", // Your super secret client token, when using replit use token: process.env.token and create a secret with your token in it instead for safety purposes.
+    "token": process.env.TOKEN, // Your super secret client token, when using replit use token: process.env.token and create a secret with your token in it instead for safety purposes.
     "prefix": "a!", // The prefix your bot will respond to, can be multiple for example ["!", "?"], so the bot would respond to ?ping and !ping
     "intents": ["Guilds", "GuildMessages", "MessageContent", "GuildVoiceStates"], // Array of intents used for your bot, list can be found here: https://aoi.js.org/docs/guides/permissionsandintents
     "events": ["onMessage", "onInteractionCreate"], // Array of events used for your bot, list can be found here: https://aoi.js.org/docs/guides/events
